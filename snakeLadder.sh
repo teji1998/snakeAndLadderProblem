79707bc9660cd48e71d67f01bb5eb44219db6af8
“teji1998” <tejasvinirpk@gmail.com>
2020-08-04 10:30:18 +0530

    Added:Functionality for the rolling of dice

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -5,3 +5,11 @@ echo "WELCOME TO SNAKE AND LADDER"
 #Initializing the constants
 START_POSITION=0;
 PLAYERS=1;
+
+#To obtain numbers 1 to 6 on dice 
+diceRoll()
+{
+	dice=$(( 1 + ($RANDOM % 6) ))
+	echo " The result of the rolling of the dice is $dice "
+}
+diceRoll
