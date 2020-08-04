2f6fc8d6ad44ca2e638244557ace3f25264035d5
“teji1998” <tejasvinirpk@gmail.com>
2020-08-04 10:55:39 +0530

    Added:Condition for exact winning position

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -8,9 +8,11 @@ PLAYERS=1;
 NO_PLAY=1;
 LADDER=2;
 SNAKE=3;
+WIN_POSITION=100;
 
 #Variables
 position=$START_POSITION
+playerTemp=0;
 
 #To obtain numbers 1 to 6 on dice 
 diceRoll()
@@ -30,23 +32,33 @@ checkOption()
 			echo "Player won't move.You will stay in same position $position."
 			;;
 		$LADDER)
-			position=$(( $position + $dice ))
-			echo "Player has got ladder.Move ahead to $position."
+			playerTemp=$(( $position + $dice ))
+			if [  $playerTemp -gt $WIN_POSITION ]
+			then 
+				position=$position;
+				echo "Player will remain in the same position $position"
+			else
+				position=$playerTemp;
+				echo "Player has got ladder.Move ahead to position $position."
+			fi
 			;;
 		$SNAKE)
-			position=$(( $position - $dice ))
-			if [ $position -lt 0 ]
+			playerTemp=$(( $position - $dice ))
+			if [ $playerTemp -lt $START_POSITION ]
 			then
 				position=$START_POSITION;
+				echo "Player will go start from beginning at position $position"
+			else
+				position=$playerTemp;
+				echo "Player has got snake.Move back to position $position."
 			fi
-			echo "Player has got snake.Move back to $position."
 			;;
 	esac
 }
-checkOption
+
 
 #To play continuously to reach 100th position
-while [ $position -lt 100 ]
+while [ $position -lt $WIN_POSITION ]
 do
 	checkOption
 done
