364c20b08c9ec03b7dc95351b6da8f353365a17e
“teji1998” <tejasvinirpk@gmail.com>
2020-08-04 11:38:09 +0530

    Added:Two player game

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -4,67 +4,117 @@ echo "WELCOME TO SNAKE AND LADDER"
 
 #Initializing the constants
 START_POSITION=0;
-PLAYERS=1;
+PLAYER_1=1;
+PLAYER_2=2;
 NO_PLAY=1;
 LADDER=2;
 SNAKE=3;
 WIN_POSITION=100;
 
 #Variables
-position=$START_POSITION
+position=0
+option=0
+dice=0
 playerTemp=0;
-diceCount=0;
+diceCountOne=0;
+diceCountTwo=0;
+playerOnePosition=0;
+playerTwoPosition=0;
 
-#To obtain numbers 1 to 6 on dice 
-diceRoll()
-{
-	dice=$(( 1 + ($RANDOM % 6) ))
-	echo " The result of the rolling of the dice is $dice "
-
-	#Count the number of times dice rolled
-	(( diceCount++ ))
-}
 
 
 #To check options for playing
 checkOption()
 {
-	option=$(( 1 + ($RANDOM % 3) ))
-	diceRoll
+	tag=$1
+	dice=$2
+	option=$3
 	case $option in
 		$NO_PLAY)
-			echo "Player won't move.You will stay in same position $position."
+			if [ $tag -eq $PLAYER_1 ]
+			then
+				playerOnePosition=$playerOnePosition
+			else
+				playerTwoPosition=$playerTwoPosition
+			fi
 			;;
 		$LADDER)
-			playerTemp=$(( $position + $dice ))
-			if [  $playerTemp -gt $WIN_POSITION ]
-			then 
-				position=$position;
-				echo "Player will remain in the same position $position"
+			if [ $tag -eq $PLAYER_1 ]
+			then
+				playerTemp=$(( $playerOnePosition + $dice ))
+				if [  $playerTemp -gt $WIN_POSITION ]
+				then 
+					playerOnePosition=$playerOnePosition;
+				else
+					playerOnePosition=$playerTemp;
+				fi
 			else
-				position=$playerTemp;
-				echo "Player has got ladder.Move ahead to position $position."
+				playerTemp=$(( $playerTwoPosition + $dice ))
+				if [  $playerTemp -gt $WIN_POSITION ]
+				then 
+					playerTwoPosition=$playerTwoPosition;
+				else
+					playerTwoPosition=$playerTemp;
+				fi
 			fi
 			;;
 		$SNAKE)
-			playerTemp=$(( $position - $dice ))
-			if [ $playerTemp -lt $START_POSITION ]
+			if [ $tag -eq $PLAYER_1 ]
 			then
-				position=$START_POSITION;
-				echo "Player will go start from beginning at position $position"
+				playerTemp=$(( $playerOnePosition - $dice ))
+				if [ $playerTemp -lt $START_POSITION ]
+				then
+					playerOnePosition=$START_POSITION;
+				else
+					playerOnePosition=$playerTemp;
+				
+				fi
 			else
-				position=$playerTemp;
-				echo "Player has got snake.Move back to position $position."
+				
+				playerTemp=$(( $playerTwoPosition - $dice ))
+				if [ $playerTemp -lt $START_POSITION ]
+				then
+					playerTwoPosition=$START_POSITION;
+				else
+					playerTwoPosition=$playerTemp;
+				
+				fi
 			fi
+				
 			;;
 	esac
+	if [ $tag -eq $PLAYER_1 ]
+	then
+		echo "Player 1 position" $playerOnePosition
+	else
+		echo "Player 2 position" $playerTwoPosition
+	fi
 }
 
-#To play continuously to reach 100th position
-while [ $position -lt $WIN_POSITION ]
-do
-	checkOption
-done
 
-#Display the number of times dice rolled
-echo "The number of times the dice was rolled is : $diceCount "
+game(){
+	while :
+	do
+		((diceCountOne++))
+	 	checkOption $PLAYER_1 $(( RANDOM % 6 + 1)) $(( RANDOM % 3 ))      #for player one
+		
+		if [ $playerOnePosition -eq $WIN_POSITION -o $playerTwoPosition -eq $WIN_POSITION ]
+		then
+				break #breaks the infinite loop if condition is true
+		fi
+
+		((diceCountTwo++))
+		checkOption $PLAYER_2 $(( RANDOM % 6 + 1)) $(( RANDOM % 3 ))      #for player two
+
+	done
+}
+
+game
+
+#to find the winner
+if [  $playerOnePosition -eq $WIN_POSITION ]
+then
+	echo " Player one has won by rolling $diceCountOne times"
+else
+	echo " Player two has won by rolling $diceCountTwo times"
+fi
